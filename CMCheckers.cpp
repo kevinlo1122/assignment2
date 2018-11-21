b017249b6a5d3f4ee22d440c1614ef11043a5829
kevinlo1122 <45241916+kevinlo1122@users.noreply.github.com>
2018-11-21 10:36:05 -0800

    Add files via upload

---
+++ CMCheckers.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+//global variables
+const int MAX_ARRAY_SIZE = 18;
+const int MIN_ARRAY_SIZE = 8;
+const int MAX_PIECES = 72;
+const int NOPLAYER = 0;
+const int WHITEWINS = 1;
+const int REDWINS = 2;
+const int NOONEWINS = 0;
+const int WHITESOLDIER = 1;
+const int WHITEMULE = 2;
+const int WHITEKING = 3;
+const int REDSOLDIER = 4;
+const int REDMULE = 5;
+const int REDKING = 6;
+const int WHITEPLAYER = 1;
+const int REDPLAYER = 2;
+/*
+//other fuctions
+void InitializeBoard(int CMCheckersBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE],
+
+	int numRowsInBoard);
+
+void DisplayBoard(int CMCheckersBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE],
+
+	int numRowsInBoard);
+
+int CountJumps(int CMCheckersBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE],
+
+	int numRowsInBoard, int player, int xLocArray[], int yLocArray[]);
+
+bool CheckList(int inArray1[], int inArray2[], int xIndex, int yIndex);
+
+int CountMove1Squares(int CMCheckersBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE],
+
+	int numRowsInBoard, int player, int xLocArray[], int yLocArray[]);
+
+bool IsMove1Square(int CMCheckersBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE],
+
+	int numRowsInBoard, int player, int xLoc, int yLoc);
+
+bool IsJump(int CMCheckersBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE],
+
+	int numRowsInBoard, int player, int xLoc, int yLoc);
+
+bool MakeMove(int CMCheckersBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE],
+
+	int numRowsInBoard, int player, int fromSquareNum,
+
+	int toSquareNum, bool &jumped);
+
+bool CheckWin(int CMCheckersBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard);
+
+bool CheckWin(int CMCheckersBoard[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE], int numRowsInBoard);
+*/
+//main function
+int main()
+{
+	//local variables
+	int numRowsInBoard = 0;
+	int numOfTries = 0; //counter for error in selecting size of board
+	//getting size of board
+	//checking if size is a valid input
+	while (numOfTries <= 3)
+	{
+		if (numOfTries > 3)
+		{
+			"ERROR: Too many erros entering the size of the board.";
+			return 1;
+		}
+		numOfTries++;
+		cout << "Enter the number of squares along each edge of the board: ";
+		if (!(cin >> numRowsInBoard))
+		{
+			cerr << "ERROR: Board size is not an integer" << endl;
+			cout << "8 <= number of squares <= 18" << endl;
+			continue;
+		}
+		if (numRowsInBoard > 18)
+		{
+			cerr << "ERROR: Board size too large." << endl;
+			cout << "8 <= number of squares <= 18" << endl;
+			continue;
+		}
+		if (numRowsInBoard < 8)
+		{
+			cerr << "ERROR: Board size too small." << endl;
+			cout << "8 <= number of squares <= 18" << endl;
+			continue;
+		}
+		if (!(numRowsInBoard % 2 == 0))
+		{
+			cerr << "ERROR: Board size is odd." << endl;
+			cout << "8 <= number of squares <= 18" << endl;
+			continue;
+		}
+		
+	}
+	return 0;
+}
